337980a2f74610fda19e1cdf8e7d6f970e04f58a
user <user@example.com>
2025-10-16 21:37:33 +0000

    updated lazygit

--- nvim/.config/nvim/lua/plugins/lazygit.lua
+++ nvim/.config/nvim/lua/plugins/lazygit.lua
@@ -11,5 +11,40 @@ return {
       "LazyGitLog",
       "LazyGitLogCurrentFile",
     },
+    keys = {
+      {
+        "<leader>gg",
+        function()
+          if vim.fn.executable("lazygit") == 0 then
+            vim.notify("lazygit executable not found", vim.log.levels.ERROR)
+            return
+          end
+          vim.cmd("LazyGit")
+        end,
+        desc = "LazyGit",
+      },
+      {
+        "<leader>gG",
+        function()
+          if vim.fn.executable("lazygit") == 0 then
+            vim.notify("lazygit executable not found", vim.log.levels.ERROR)
+            return
+          end
+          vim.cmd("LazyGitCurrentFile")
+        end,
+        desc = "LazyGit (Current File)",
+      },
+      {
+        "<leader>gH",
+        function()
+          if vim.fn.executable("lazygit") == 0 then
+            vim.notify("lazygit executable not found", vim.log.levels.ERROR)
+            return
+          end
+          vim.cmd("LazyGitFilterCurrentFile")
+        end,
+        desc = "LazyGit: File History",
+      },
+    },
   },
 }
