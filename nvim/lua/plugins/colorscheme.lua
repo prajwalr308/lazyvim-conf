e61c6e139051688ec5703ee83cfd8fdf7edd8689
user <user@example.com>
2025-10-16 21:07:03 +0000

    updated nvim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -19,6 +19,46 @@ return {
         notify = false,
       },
     },
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+    end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "moon",
+      styles = {
+        comments = { italic = true },
+        keywords = { italic = true },
+      },
+      sidebars = "dark",
+      dim_inactive = true,
+    },
+    config = function(_, opts)
+      require("tokyonight").setup(opts)
+    end,
+  },
+  {
+    "navarasu/onedark.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "darker",
+      code_style = {
+        comments = "italic",
+        keywords = "bold",
+      },
+      diagnostics = {
+        darker = true,
+        undercurl = true,
+        background = false,
+      },
+    },
+    config = function(_, opts)
+      require("onedark").setup(opts)
+    end,
   },
   {
     "LazyVim/LazyVim",
---
+++ nvim/lua/plugins/formatting.lua
@@ -0,0 +1,4 @@
+return {
+  -- import LazyVim's Prettier integration (Conform + Mason)
+  { import = "lazyvim.plugins.extras.formatting.prettier" },
+}
